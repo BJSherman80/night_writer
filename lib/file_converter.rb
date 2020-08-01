58c75a896fe36cba4b39e4943d481cf24a829dd6
Brett Turing <bjsherman80@gmail.com>
2020-07-31 22:08:07 -0600

    Created file converter

--- lib/file_converter.rb
+++ lib/file_converter.rb
@@ -1,11 +1,46 @@
 
+require './lib/reader'
+
 class FileConverter
 
-  def read
-    filename = ARGV[0]
-    File.read(filename)
+  def initialize(input, output)
+    @input  = input
+    @output = output
+    message
   end
 
-  
+  def read
+    File.read(@input)
+  end
 
+  def message
+    puts "Created #{@output} containing #{@input.length} characters."
+  end
+
+
+# class FileConverter
+#
+#   def initialize(input, output)
+#     @reader = Reader.new
+#     @input  = @reader.read
+#     @output = output
+#     message
+#   end
+#
+#   def message
+#     puts "Created #{@output} containing #{@input.length} characters."
+#   end
+
+  # def encode_file_to_braille
+  #   # I wouldn't worry about testing this method
+  #   # unless you get everything else done
+  #   plain = reader.read
+  #   braille = encode_to_braille(plain)
+  # end
+  #
+  # def encode_to_braille(input)
+  #   # you've taken in an INPUT string
+  #   # do the magic
+  #   # send out an OUTPUT string
+  # end
 end
