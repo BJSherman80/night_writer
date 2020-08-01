2ac6c5397ea4830b32af0e483be87100e7947a81
Brett Turing <bjsherman80@gmail.com>
2020-08-01 13:37:16 -0600

    Update translator test

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -20,7 +20,14 @@ class TranslatorTest < MiniTest::Test
 
   def test_it_can_translate_a_letter
     translator = Translator.new
+
     assert_equal ["0.", "..", ".."], translator.translate_letter("a")
+  end
+
+  def test_it_can_translate_word
+    translator = Translator.new
+
+    assert_equal [["00", "..", ".."],["0.", "..", ".."],[".0", "00", "0."]], translator.translate_word("cat")
   end
 
 end
