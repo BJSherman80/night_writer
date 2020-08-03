acb545bfb901ba0d714ab21bd84ab9b2390662fb
Brett Turing <bjsherman80@gmail.com>
2020-08-02 21:58:03 -0600

    Created test it can chunk doc

---
+++ test/file_converter_test.rb
@@ -0,0 +1,48 @@
+require './test/test_helper'
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/file_converter'
+require 'pry'
+
+class FileConverterTest < MiniTest::Test
+
+  def test_it_exists
+   input = "message.txt"
+   output = "braille.txt"
+   ARGV[0] = input
+   ARGV[1] = output
+   night_writer = FileConverter.new(input, output)
+   assert_instance_of FileConverter, night_writer
+  end
+
+  def test_it_can_read_txt_docs
+    output = "braille.txt"
+    input = "message1.txt"
+    file_converter = FileConverter.new(input, output)
+    assert_equal "hello world", file_converter.input_text
+  end
+
+
+  def test_it_can_write_docs
+    skip
+    output = "braille.txt"
+    input = "message1.txt"
+    @input_text.stubs(write).returns("0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n")
+    braile = mock('text document')
+    night_writer = FileConverter.new(input, output)
+    assert_equal "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n", night_writer.write
+  end
+
+  def test_it_can_chunk_doc
+    output = "braille.txt"
+    input = "message.txt"
+    night_writer = FileConverter.new(input, output)
+    assert_equal "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n bb\n", night_writer.input_chunk
+  end
+
+
+
+
+end
